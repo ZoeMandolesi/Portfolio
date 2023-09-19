5e4e4f596345017c6a085b59885eb61e054deb7d
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-09-19 10:52:40 -0300

    fixed linters

--- test/controllers/certifications_controller_test.rb
+++ test/controllers/certifications_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class CertificationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get certifications_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get certifications_show_url
     assert_response :success
   end
