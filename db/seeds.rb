e39712759eb7d70c278e74fc3e94d65f52f15ff1
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-09-26 17:14:12 -0300

    added welcone page code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,56 +1,57 @@
-# Project.create(
-#   title: "EasyClinic API",
-#   overview: "This meticulously crafted back-end application streamlines appointment scheduling for patients with a diverse network of healthcare professionals in the healthcare industry.",
-#   start_date: Date.new(2023, 8, 24),
-#   end_date: Date.new(2023, 9, 7),
-#   github_url: "https://github.com/federicaulzurrun/final-capstone-back-end",
-#   demo_url: "https://rails-xdb0.onrender.com/api-docs/index.html",
-#   technologies: "Ruby on Rails",
-#   details: "EasyClinic serves as the backbone of a comprehensive healthcare appointment booking system. It offers a user-friendly interface, enabling patients to effortlessly secure appointments with doctors and specialists across an extensive health network. The application incorporates advanced backend functionalities to ensure smooth and efficient appointment management."
-# )
+Project.create(
+  title: "EasyClinic API",
+  overview: "This meticulously crafted back-end application streamlines appointment scheduling for patients with a diverse network of healthcare professionals in the healthcare industry.",
+  start_date: Date.new(2023, 8, 24),
+  end_date: Date.new(2023, 9, 7),
+  github_url: "https://github.com/federicaulzurrun/final-capstone-back-end",
+  demo_url: "https://rails-xdb0.onrender.com/api-docs/index.html",
+  technologies: "Ruby on Rails",
+  details: "EasyClinic serves as the backbone of a comprehensive healthcare appointment booking system. It offers a user-friendly interface, enabling patients to effortlessly secure appointments with doctors and specialists across an extensive health network. The application incorporates advanced backend functionalities to ensure smooth and efficient appointment management."
+)
 
-# Project.create(
-#   title: "PennyLane",
-#   overview: "PennyLane is a web application that simplifies personal finance management, allowing users to track expenses and budget effectively.",
-#   start_date: Date.new(2023, 8, 14),
-#   end_date: Date.new(2023, 8, 17),
-#   github_url: "https://github.com/federicaulzurrun/PennyLane",
-#   demo_url: "https://rails-08wi.onrender.com/",
-#   technologies: "Ruby on Rails",
-#   details: "PennyLane empowers users to take control of their financial well-being by providing a user-friendly platform for expense tracking and budget management. With intuitive features, it enables users to categorize expenses, set budget goals, and gain valuable insights into their spending habits. The application is built on the Ruby on Rails framework, ensuring security, reliability, and a seamless user experience."
-# )
+Project.create(
+  title: "PennyLane",
+  overview: "PennyLane is a web application that simplifies personal finance management, allowing users to track expenses and budget effectively.",
+  start_date: Date.new(2023, 8, 14),
+  end_date: Date.new(2023, 8, 17),
+  github_url: "https://github.com/federicaulzurrun/PennyLane",
+  demo_url: "https://rails-08wi.onrender.com/",
+  technologies: "Ruby on Rails",
+  details: "PennyLane empowers users to take control of their financial well-being by providing a user-friendly platform for expense tracking and budget management. With intuitive features, it enables users to categorize expenses, set budget goals, and gain valuable insights into their spending habits. The application is built on the Ruby on Rails framework, ensuring security, reliability, and a seamless user experience."
+)
 
-# Project.create(
-#   title: "Blogify",
-#   overview: "Blogify is a versatile blogging platform that empowers users to create and share engaging content with ease.",
-#   start_date: Date.new(2023, 8, 3),
-#   end_date: Date.new(2023, 8, 5),
-#   github_url: "https://github.com/federicaulzurrun/Blogify",
-#   technologies: "Ruby on Rails",
-#   details: "Blogify is the perfect solution for bloggers and content creators looking to establish a digital presence. With its intuitive interface, users can effortlessly create, edit, and publish blog posts. Features include rich text editing, image uploads, and customizable themes. Built on the Ruby on Rails framework, Blogify ensures security, scalability, and a seamless blogging experience for both novice and experienced writers."
-# )
+Project.create(
+  title: "Blogify",
+  overview: "Blogify is a versatile blogging platform that empowers users to create and share engaging content with ease.",
+  start_date: Date.new(2023, 8, 3),
+  end_date: Date.new(2023, 8, 5),
+  github_url: "https://github.com/federicaulzurrun/Blogify",
+  technologies: "Ruby on Rails",
+  details: "Blogify is the perfect solution for bloggers and content creators looking to establish a digital presence. With its intuitive interface, users can effortlessly create, edit, and publish blog posts. Features include rich text editing, image uploads, and customizable themes. Built on the Ruby on Rails framework, Blogify ensures security, scalability, and a seamless blogging experience for both novice and experienced writers."
+)
 
-# Project.create(
-#   title: "Catalogue of My Things",
-#   overview: "Catalogue of My Things is a Ruby object-oriented programming console app designed to help you record various types of possessions, including books, music albums, movies, and games.",
-#   start_date: Date.new(2023, 9, 10),
-#   end_date: Date.new(2023, 7, 14),
-#   github_url: "https://github.com/federicaulzurrun/Catalogue-of-my-things",
-#   technologies: "Ruby",
-#   details: "Catalogue of My Things is a versatile console application that simplifies the process of cataloging your personal belongings. Whether you own a diverse collection of books, music albums, movies, or games, this app provides an organized and user-friendly platform to record and manage your possessions. Built with Ruby and following an object-oriented programming paradigm, it ensures efficient data handling and a seamless user experience."
-# )
+Project.create(
+  title: "Catalogue of My Things",
+  overview: "Catalogue of My Things is a Ruby object-oriented programming console app designed to help you record various types of possessions, including books, music albums, movies, and games.",
+  start_date: Date.new(2023, 9, 10),
+  end_date: Date.new(2023, 7, 14),
+  github_url: "https://github.com/federicaulzurrun/Catalogue-of-my-things",
+  technologies: "Ruby",
+  details: "Catalogue of My Things is a versatile console application that simplifies the process of cataloging your personal belongings. Whether you own a diverse collection of books, music albums, movies, or games, this app provides an organized and user-friendly platform to record and manage your possessions. Built with Ruby and following an object-oriented programming paradigm, it ensures efficient data handling and a seamless user experience."
+)
 
-# Project.create(
-#   title: "SPACE TRAVELER'S HUB",
-#   overview: "This project is a web application built using React and Redux, consuming the SpaceX data API. This page allows the user to see the available rockets and missions. Also, the user can join one or all of them. In the My Profile section, the user can see the reservations they have made.",
-#   start_date: Date.new(2023, 5, 19),
-#   end_date: Date.new(2023, 5, 24),
-#   github_url: "https://github.com/federicaulzurrun/react-group-project",
-#   technologies: "React, Redux",
-#   details: "The project features a visually stunning user interface with real-time updates of SpaceX missions. Users can explore details about rockets, missions, and even track upcoming launches. The backend is powered by a robust Ruby on Rails API, ensuring fast and reliable data retrieval. Additionally, the application employs user authentication for secure reservations, and users can share their reservations on social media."
-# )
+Project.create(
+  title: "SPACE TRAVELER'S HUB",
+  overview: "This project is a web application built using React and Redux, consuming the SpaceX data API. This page allows the user to see the available rockets and missions. Also, the user can join one or all of them. In the My Profile section, the user can see the reservations they have made.",
+  start_date: Date.new(2023, 5, 19),
+  end_date: Date.new(2023, 5, 24),
+  github_url: "https://github.com/federicaulzurrun/react-group-project",
+  demo_url: "https://deluxe-starlight-0ec1fe.netlify.app/rockets",
+  technologies: "React, Redux",
+  details: "The project features a visually stunning user interface with real-time updates of SpaceX missions. Users can explore details about rockets, missions, and even track upcoming launches. The backend is powered by a robust Ruby on Rails API, ensuring fast and reliable data retrieval. Additionally, the application employs user authentication for secure reservations, and users can share their reservations on social media."
+)
 
-#Skills
+Skills
 
 Skill.create(name: "JavaScript", category: "Languages")
 Skill.create(name: "Ruby", category: "Languages")
