87bd923350bb2791e6dcb5d7b5819cb12db5b772
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-09-26 22:19:56 -0300

    added style to mobile view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,20 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menu-toggle');
+  const mobileMenu = document.getElementById('mobileMenu');
+  const mobileMenuLinks = mobileMenu.querySelectorAll('a.navLink'); // Select all links in the mobile menu
+
+  // Function to hide the mobile menu
+  function hideMobileMenu() {
+    mobileMenu.classList.add('hidden');
+  }
+
+  // Add a click event listener to each link in the mobile menu
+  mobileMenuLinks.forEach(link => {
+    link.addEventListener('click', hideMobileMenu);
+  });
+
+  // Toggle the mobile menu when the menu-toggle button is clicked
+  menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
+});
