d5237b6306a7a4db1aeaf5db54161cdcdc310167
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-09-27 12:52:19 -0300

    fixed logic on ham menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,19 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuToggle = document.getElementById('menu-toggle');
   const mobileMenu = document.getElementById('mobileMenu');
-  const mobileMenuLinks = mobileMenu.querySelectorAll('a.navLink'); // Select all links in the mobile menu
+  const mobileMenuLinks = mobileMenu.querySelectorAll('a.navLink'); 
 
-  // Function to hide the mobile menu
-  function hideMobileMenu() {
+  function showMobileMenu() {
     mobileMenu.classList.add('hidden');
   }
 
-  // Add a click event listener to each link in the mobile menu
+  function hideMobileMenu() {
+    mobileMenu.classList.remove('hidden');
+  }
+
+  mobileMenuLinks.forEach(link => {
+    link.addEventListener('click', showMobileMenu);
+  });
+
   mobileMenuLinks.forEach(link => {
     link.addEventListener('click', hideMobileMenu);
   });
 
-  // Toggle the mobile menu when the menu-toggle button is clicked
   menuToggle.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
   });
